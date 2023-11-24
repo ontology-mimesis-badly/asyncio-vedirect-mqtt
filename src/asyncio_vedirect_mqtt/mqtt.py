8ca8704da166e6bef35b5d49add637c527fa6cc1
ontology-mimesis-badly <github@accounts.hobf.io>
2023-11-24 17:30:51 +1100

    add device types

--- src/asyncio_vedirect_mqtt/mqtt.py
+++ src/asyncio_vedirect_mqtt/mqtt.py
@@ -9,11 +9,90 @@ import time
 
 logger = logging.getLogger(__name__)
 
+device_types = {
+    "mppt": {
+        "model": "BlueSolar 100/50",
+        "category": "Solar",
+        "sensors": {
+            "H19": {
+                "name": "Yield Total",
+                "unit_of_measurement": "kWh",
+                "device_class": "energy",
+                "state_class": "total_increasing",
+                "multiplier": 0.01
+            },
+            "H20": {
+                "name": "Yield Today",
+                "unit_of_measurement": "kWh",
+                "device_class": "energy",
+                "state_class": "total_increasing",
+                "multiplier": 0.01
+            },
+            "V": {
+                "name": "Output Voltage",
+                "unit_of_measurement": "V",
+                "device_class": "voltage",
+                "state_class": "measurement",
+                "multiplier": 0.001
+            },
+            "VPV": {
+                "name": "Panel Voltage",
+                "unit_of_measurement": "V",
+                "device_class": "voltage",
+                "state_class": "measurement",
+                "multiplier": 0.001
+            },
+            "I": {
+                "name": "Output Current",
+                "unit_of_measurement": "A",
+                "device_class": "current",
+                "state_class": "measurement",
+                "multiplier": 0.001
+            },
+            # "IL": {
+            #     "name": "Load Current",
+            #     "unit_of_measurement": "A",
+            #     "device_class": "current",
+            #     "state_class": "measurement",
+            #     "multiplier": 0.001
+            # },
+            "PPV": {
+                "name": "Panel Power",
+                "unit_of_measurement": "W",
+                "device_class": "power",
+                "state_class": "measurement",
+                "multiplier": 1
+            },
+        }
+    },
+    "shunt": {
+        "model": "SmartShunt 500A/50mV",
+        "category": "Battery",
+        "sensors": {
+            "V": {
+                "name": "Battery Voltage",
+                "unit_of_measurement": "V",
+                "device_class": "voltage",
+                "state_class": "measurement",
+                "multiplier": 0.001
+            },
+            "I": {
+                "name": "Battery Current",
+                "unit_of_measurement": "A",
+                "device_class": "current",
+                "state_class": "measurement",
+                "multiplier": 0.001
+            },
+        }
+    }
+}
+
 class AsyncIOVeDirectMqtt:
-    def __init__(self, tty, device, broker, *args, tls_protocol=None, windowing=60, port=1883, username=None, password=None, mqttretry=30, verbose=False, timeout=60,
+    def __init__(self, tty, device, type, broker, *args, tls_protocol=None, windowing=60, port=1883, username=None, password=None, mqttretry=30, verbose=False, timeout=60,
                  ca_path=None, **kwargs):
         self.tty = tty
         self.device_name = device
+        self.device_type = device_types[type]
         self.broker = broker
         self.port = int(port)
         self.username = username
@@ -30,83 +109,22 @@ class AsyncIOVeDirectMqtt:
 
     async def setup_sensors(self, mqtt_client):
         logger.info("Publishing sensors to home assistant")
-        device = Device(self.device_name, 'SmartSolar 75/10', 'Victron')
-        self.sensor_mapping["H19"] = Sensor(
-            mqtt_client,
-            "Yield Total",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="kWh",
-            device_class="energy",
-            state_class="total_increasing",
-            multiplier=0.01,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["H20"] = Sensor(
-            mqtt_client,
-            "Yield Today",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="kWh",
-            device_class="energy",
-            state_class="total_increasing",
-            multiplier=0.01,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["V"] = Sensor(
-            mqtt_client,
-            "Battery Voltage",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="V",
-            device_class="voltage",
-            state_class="measurement",
-            multiplier=0.001,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["VPV"] = Sensor(
-            mqtt_client,
-            "Panel Voltage",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="V",
-            device_class="voltage",
-            state_class="measurement",
-            multiplier=0.001,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["I"] = Sensor(
-            mqtt_client,
-            "Battery Current",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="A",
-            device_class="current",
-            state_class="measurement",
-            multiplier=0.001,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["IL"] = Sensor(
-            mqtt_client,
-            "Load Current",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="A",
-            device_class="current",
-            state_class="measurement",
-            multiplier=0.001,
-            mov_avg=self.windowing
-        )
-        self.sensor_mapping["PPV"] = Sensor(
-            mqtt_client,
-            "Panel Power",
-            category="Solar",
-            parent_device=device,
-            unit_of_measurement="W",
-            device_class="power",
-            state_class="measurement",
-            mov_avg=self.windowing
-        )
+        device = Device(self.device_name, self.device_type["model"], 'Victron')
+
+        for sensor_id in self.device_type["sensors"]:
+            sensor = self.device_type["sensors"][sensor_id]
+            self.sensor_mapping[sensor_id] = Sensor(
+                mqtt_client,
+                sensor["name"],
+                category=self.device_type["category"],
+                parent_device=device,
+                unit_of_measurement=sensor["unit_of_measurement"],
+                device_class=sensor["device_class"],
+                state_class=sensor["state_class"],
+                multiplier=sensor["multiplier"],
+                mov_avg=self.windowing
+            )
+
         for key in self.sensor_mapping.keys():
             await self.sensor_mapping[key].publish_discovery()
 
